a33b7cd9455ad21f9b1121c5aac6cf2e7b711828
Benjamin Marks <ben@blueacorn.com>
2013-02-08 16:28:43 -0600

    whitespace

--- app/code/local/Training/Distro/Block/Adminhtml/Distributor/Edit/Tabs.php
+++ app/code/local/Training/Distro/Block/Adminhtml/Distributor/Edit/Tabs.php
@@ -1,31 +1,31 @@
 <?php
 
 class Training_Distro_Block_Adminhtml_Distributor_Edit_Tabs
-	extends Mage_Adminhtml_Block_Widget_Tabs
+    extends Mage_Adminhtml_Block_Widget_Tabs
 {
-	protected function _construct()
-	{
-		parent::_construct();
-		$this->setId('switch_tabs');
-		$this->setDestElementId('edit_form');
-		$this->setTitle($this->__('Distributor Setup'));
-	}
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setId('switch_tabs');
+        $this->setDestElementId('edit_form');
+        $this->setTitle($this->__('Distributor Setup'));
+    }
 
-	/**
-	 * Add tabs content
-	 *
-	 * @return Training_Distro_Block_Adminhtml_Distributor_Edit_Tabs
-	 */
-	protected function _beforeToHtml()
-	{
-		$this->addTab('general_section', array(
-				'label'   => $this->__('General'),
-				'title'   => $this->__('General'),
-				'content' => $this->getLayout()
-					->createBlock('training_distro/adminhtml_distributor_edit_tab_general')
-					->toHtml(),
-		));
+    /**
+     * Add tabs content
+     *
+     * @return Training_Distro_Block_Adminhtml_Distributor_Edit_Tabs
+     */
+    protected function _beforeToHtml()
+    {
+        $this->addTab('general_section', array(
+            'label' => $this->__('General'),
+            'title' => $this->__('General'),
+            'content' => $this->getLayout()
+                ->createBlock('training_distro/adminhtml_distributor_edit_tab_general')
+                ->toHtml(),
+        ));
 
-		return parent::_beforeToHtml();
-	}
+        return parent::_beforeToHtml();
+    }
 }
